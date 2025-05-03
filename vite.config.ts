cd3888088ed56d5434b39754809968fbff3cb590
wandersonneres <36671464+wandersonneres@users.noreply.github.com>
2025-05-03 02:24:03 -0300

    subindo novamente
    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-    manifest: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-    }
-  },
-  base: '',
+    outDir: 'dist'
+  }
 }); 
