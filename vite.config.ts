8f6f673eb68beaab392938a4bebbee5e5d390b25
wandersonneres <36671464+wandersonneres@users.noreply.github.com>
2025-05-03 02:17:28 -0300

    subindo

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +11,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    manifest: true,
     rollupOptions: {
-      input: './index.html',
+      input: resolve(__dirname, 'index.html'),
     }
   },
-  base: './',
+  base: '',
 }); 
